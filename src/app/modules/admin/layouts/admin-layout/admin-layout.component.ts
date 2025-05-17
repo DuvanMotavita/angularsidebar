f0eb105510a052c52c6df07f54468ba3767ba9ff
DuvanMotavita <duvanfel1@yahoo.com.co>
2025-05-17 17:17:32 -0500

    Added new functionality to get correct side bar attributes

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,18 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApxSideMenuComponent,TitleColor } from 'apx-side-menu-duvan27';
 import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
-
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet,SideMenuComponent],
+  imports: [RouterOutlet,SideMenuComponent,ApxSideMenuComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
 export default class AdminLayoutComponent {
 
   isAuthenticated = signal(false);
-
+  TitleColor = TitleColor;
   onLogin(){
     this.isAuthenticated.set(true);
   };
